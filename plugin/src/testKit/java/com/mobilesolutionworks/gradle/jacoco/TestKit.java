d55b1911e4665b24f306f1cf58968dd249e3307f
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-08-06 16:05:11 +0800

    Revise idle timeout of daemon

--- plugin/src/testKit/java/com/mobilesolutionworks/gradle/jacoco/TestKit.java
+++ plugin/src/testKit/java/com/mobilesolutionworks/gradle/jacoco/TestKit.java
@@ -30,8 +30,6 @@ public class TestKit {
             properties.setProperty("org.gradle.jvmargs", agentString);
             properties.setProperty("org.gradle.daemon", "false");
             properties.setProperty("org.gradle.parallel", "false");
-            properties.setProperty("org.gradle.daemon.idletimeout", "1000");
-            properties.setProperty("org.gradle.workers.max", "1");
 
             properties.store(new FileOutputStream(new File(rootDir, "gradle.properties")), "Gradle");
         }
